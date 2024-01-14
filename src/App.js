cc2828ba0e0ba7d0aa453e352fd62f2370bafb17
ROZZYNUEZ14 <palacinrossy@gmail.com>
2024-01-14 18:28:23 -0500

    creacion de la vista home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import  { BrowserRouter, Link, Routes, Route} from "react-router-dom"
-import { Home } from "./Home"
+import { Home } from "./vistas/Home"
 import { Hola } from "./components"
 
 //const Home = () => <h1 style={{ backgroundColor: 'red' }}>JJHJJJ</h1>
